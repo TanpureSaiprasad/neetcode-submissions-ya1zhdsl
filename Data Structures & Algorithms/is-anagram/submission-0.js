33a821c613ea59fd879863bca127be9447813236
Tanpure Saiprasad Ramesh <138004794+TanpureSaiprasad@users.noreply.github.com>
2026-04-11 20:09:53 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length)
+            return false;
+        
+        let s1 = s.split("").sort().join();
+        let t1 = t.split("").sort().join();
+
+        return s1 === t1;        
+    }
+}
