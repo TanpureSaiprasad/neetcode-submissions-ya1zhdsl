b33a6c82bc2ec91934bc2b375c6129b32fefc3bd
Tanpure Saiprasad Ramesh <138004794+TanpureSaiprasad@users.noreply.github.com>
2026-04-17 18:49:30 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let out=[];
+
+        for(let i = 0; i < nums.length; i++){
+            let product=1;
+            for(let j = 0; j < nums.length; j++){
+                if(j==i)continue;
+                product *= nums[j];
+            }
+            out.push(product);
+            product=1;
+        }
+        return out;
+    }
+}
